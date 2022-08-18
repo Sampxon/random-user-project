e6fd82f5181ef319c02a2f6ee3f4ed60f96fbf93
anonxbt <108493672+anonxbt@users.noreply.github.com>
2022-08-18 21:04:44 +0100

    new

--- random.js
+++ random.js
@@ -1,10 +1,14 @@
-const getElement = (selection) => {
-  const element = document.querySelector(selection);
-  if (element) {
-    return element;
-  } else {
-    throw new Error('no element selected');
-  }
-};
+// const getElement = (selection) => {
+//   const element = document.querySelector(selection);
+//   if (element) {
+//     return element;
+//   } else {
+//     throw new Error('no element selected');
+//   }
+// };
+import getElement from './utils/global-functions.js';
 
 const img = getElement('.user-img');
+const title = getElement('.user-title');
+const value = getElement('.user-value');
+const btn = getElement('.btn');
