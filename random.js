9c0c2997434ac590ad8e7df4b9b1da2e50488194
anonxbt <108493672+anonxbt@users.noreply.github.com>
2022-08-18 22:15:30 +0100

    updated

--- random.js
+++ random.js
@@ -12,3 +12,24 @@ const img = getElement('.user-img');
 const title = getElement('.user-title');
 const value = getElement('.user-value');
 const btn = getElement('.btn');
+const allBtns = [...document.querySelectorAll('.icon')];
+const URL = 'https://randomuser.me/api/';
+
+//fetch
+const fetchUser = async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  //!destructure
+  const person = data.results[0];
+  console.log(person);
+};
+fetchUser();
+
+const showUser = () => {
+  //get user from api
+  //display user
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
